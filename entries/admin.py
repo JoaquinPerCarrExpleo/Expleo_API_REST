86169a0899fea7896172f4ef21e761ed253e58c4
Joaquin Perez <joaquin.perez@expleogroup.com>
2022-05-25 17:28:19 +0200

    Añadidos PUT y DELETE

--- entries/admin.py
+++ entries/admin.py
@@ -1,3 +1,6 @@
+from tkinter import Entry
 from django.contrib import admin
+from entries.models import EntryModel
 
 # Register your models here.
+admin.site.register(EntryModel)
---
+++ entries/api/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from rest_framework import serializers
+from entries.models import EntryModel
+
+class EntrySerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    concept = serializers.CharField()
+    amount = serializers.FloatField()
+    datetime = serializers.DateTimeField()
+    
+    def create(self, validated_data):
+        #Voy a salvar a validated data
+        instance = EntryModel(
+            datetime = validated_data.get("datetime"),
+            concept = validated_data.get("concept"),
+            amount = validated_data.get("amount")
+        )
+        instance.save()
+        return instance
+    
+    def update(self,instance, validated_data):
+        #Voy a modificar la isntancia con validated data
+        #pass
+        instance.datetime = validated_data.get("datetime")
+        instance.concept = validated_data.get("concept")    
+        instance.amount = validated_data.get("amount")    
+
+        instance.save()    
+        return instance
